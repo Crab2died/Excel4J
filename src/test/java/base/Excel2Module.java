256a6ed8c78c424ecbe34178193b39e66bfe8d31
WBingHE <hwb9588@126.com>
2017-06-16 15:59:48 +0800

    v2.0.0正式上线

--- src/test/java/base/Excel2Module.java
+++ src/test/java/base/Excel2Module.java
@@ -28,14 +28,16 @@ public class Excel2Module {
 
     @Test
     public void excel2Object2() throws Exception {
+        
         String path = "D:\\IdeaSpace\\Excel4J\\src\\test\\resource\\students_02.xlsx";
 
+        // 不基于注解,将Excel内容读至List<List<String>>对象内
         List<List<String>> lists = ExcelUtils.getInstance().readExcel2List(path, 1, 3, 0);
         System.out.println("读取Excel至String数组：");
         for (List<String> list : lists) {
             System.out.println(list);
         }
-
+        // 基于注解,将Excel内容读至List<Student2>对象内
         List<Student2> students = ExcelUtils.getInstance().readExcel2Objects(path, Student2.class, 0);
         System.out.println("读取Excel至对象数组(支持类型转换)：");
         for (Student2 st : students) {
