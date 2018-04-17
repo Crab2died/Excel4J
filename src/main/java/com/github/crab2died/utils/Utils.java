d5b3aa67e926af9fe0cf01f4ba9179a7d5a0d223
daihy <dai875939260@163.com>
2018-04-17 11:58:18 +0800

    Update Utils.java
    支持字段是BigDecimal类型的导入

--- src/main/java/com/github/crab2died/utils/Utils.java
+++ src/main/java/com/github/crab2died/utils/Utils.java
@@ -174,6 +174,9 @@ public class Utils {
             strField = matchDoneBigDecimal(strField);
             return Double.parseDouble(strField);
         }
+        if(BigDecimal.class == clazz) {
+            return new BigDecimal(strField);
+        }
         if ((Character.class == clazz) || (char.class == clazz)) {
             return strField.toCharArray()[0];
         }
