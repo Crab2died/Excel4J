790deba0ac2eb8e12ee472abb2c851b267f609d5
Crab2Died <hwb9588@126.com>
2020-04-01 10:02:53 +0800

    v3.0.0 unit tests

--- src/test/java/base/Excel2Module.java
+++ src/test/java/base/Excel2Module.java
@@ -8,7 +8,6 @@ import modules.StudentScore;
 import org.junit.Test;
 
 import java.nio.file.Paths;
-import java.util.Collections;
 import java.util.List;
 
 public class Excel2Module {
@@ -16,7 +15,7 @@ public class Excel2Module {
     @Test
     public void excel2Object() throws Exception {
 
-        String path = "D:\\JProject\\Excel4J\\src\\test\\resources\\students_01.xlsx";
+        String path = "./src/test/resources/students_01.xlsx";
 
         System.out.println("读取全部：");
         List<Student1> students = ExcelUtils.getInstance().readExcel2Objects(path, Student1.class);
@@ -33,7 +32,7 @@ public class Excel2Module {
     @Test
     public void excel2Object2() {
 
-        String path = "D:\\JProject\\Excel4J\\src\\test\\resources\\students_02.xlsx";
+        String path = "./src/test/resources/students_02.xlsx";
         try {
 
             // 1)
@@ -63,7 +62,7 @@ public class Excel2Module {
     @Test
     public void testReadConverter() throws Exception {
 
-        String path = "D:\\JProject\\Excel4J\\src\\test\\resources\\students_02.xlsx";
+        String path = "./src/test/resources/students_02.xlsx";
         List<Student2> students = ExcelUtils.getInstance().readExcel2Objects(path, Student2.class, 0, 0);
         System.out.println("读取Excel至对象数组(支持类型转换)：");
         for (Student2 st : students) {
@@ -74,7 +73,7 @@ public class Excel2Module {
     // 测试读取带有公式的单元格，并返回公式的值
     @Test
     public void testReadExcel_XLS() throws Exception {
-        String path = "D:\\JProject\\Excel4J\\src\\test\\resources\\StudentScore.xlsx";
+        String path = "./src/test/resources/StudentScore.xlsx";
         System.out.println(Paths.get(path).toUri().getPath());
         List<StudentScore> projectExcelModels = ExcelUtils.getInstance().readExcel2Objects(path, StudentScore.class);
         System.out.println(projectExcelModels);
@@ -83,7 +82,7 @@ public class Excel2Module {
     // 测试读取CSV文件
     @Test
     public void testReadCSV() throws Excel4JException {
-        List<Student2> list = ExcelUtils.getInstance().readCSV2Objects("J.csv", Student2.class);
+        List<Student2> list = ExcelUtils.getInstance().readCSV2Objects("./src/test/resources/students_02.csv", Student2.class);
         for (Student2 student2 : list) {
             System.out.println(student2);
         }
